74be1d72f08c154ae24f22dfd5b643dd9ec15c1f
Yashas H Majmudar <yashashm.dev@gmail.com>
2026-06-21 00:30:07 -0400

    fix: improve caret arrow

--- lib/views/gallery/components/caret_arrows.dart
+++ lib/views/gallery/components/caret_arrows.dart
@@ -1,3 +1,4 @@
+import 'package:echo_frame/constants/constants.dart' show Sizes;
 import 'package:echo_frame/theme/theme.dart';
 import 'package:echo_frame/utilities/utilities.dart';
 import 'package:flutter/material.dart';
@@ -10,9 +11,9 @@ class CaretArrow extends StatefulWidget {
     this.left = false,
   });
 
-  final bool left;
   final VoidCallback onPressed;
   final bool loadingNext;
+  final bool left;
 
   @override
   State<CaretArrow> createState() => _CaretArrowState();
@@ -24,47 +25,67 @@ class _CaretArrowState extends State<CaretArrow> {
 
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-      left: widget.left ? 0 : null,
-      right: widget.left ? null : 0,
-      top: 0,
-      bottom: 0,
-      width: _zoneWidth,
-      child: MouseRegion(
-        onEnter: (_) => setState(() => _hovering = true),
-        onExit: (_) => setState(() => _hovering = false),
-        child: AnimatedOpacity(
-          opacity: _hovering ? 1.0 : 0.0,
-          duration: Durations.short3,
-          child: IgnorePointer(
-            ignoring: !_hovering,
-            child: Material(
-              color: KnownColors.transparent,
-              child: InkWell(
-                mouseCursor: SystemMouseCursors.click,
-                onTap: widget.onPressed,
-                hoverColor: KnownColors.transparent,
-                splashColor: context.colors.onPrimary.withValues(alpha: 0.3),
-                child: Container(
-                  width: _zoneWidth,
-                  height: double.infinity,
-                  color: context.colors.surfacePrimary.withValues(alpha: 0.1),
-                  child: Center(
-                    child: widget.loadingNext && !widget.left
-                        ? SizedBox(
-                            height: 20,
-                            width: 20,
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation(
-                                  context.colors.onPrimary),
-                            ),
-                          )
-                        : Icon(
-                            widget.left
-                                ? Icons.arrow_back_ios_rounded
-                                : Icons.arrow_forward_ios_rounded,
-                            size: 25,
-                          ),
+    final colors = context.colors;
+
+    return Align(
+      alignment: widget.left ? Alignment.centerLeft : Alignment.centerRight,
+      child: SizedBox(
+        width: _zoneWidth,
+        height: context.height * 0.55,
+        child: MouseRegion(
+          onEnter: (_) => setState(() => _hovering = true),
+          onExit: (_) => setState(() => _hovering = false),
+          child: AnimatedOpacity(
+            opacity: _hovering ? 1.0 : 0.0,
+            duration: Durations.short3,
+            child: IgnorePointer(
+              ignoring: !_hovering,
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: widget.left
+                      ? Radius.zero
+                      : Radius.circular(Sizes.maxFinite),
+                  bottomLeft: widget.left
+                      ? Radius.zero
+                      : Radius.circular(Sizes.maxFinite),
+                  topRight: widget.left
+                      ? Radius.circular(Sizes.maxFinite)
+                      : Radius.zero,
+                  bottomRight: widget.left
+                      ? Radius.circular(Sizes.maxFinite)
+                      : Radius.zero,
+                ),
+                child: Material(
+                  color: KnownColors.transparent,
+                  child: InkWell(
+                    mouseCursor: SystemMouseCursors.click,
+                    onTap: widget.onPressed,
+                    hoverColor: KnownColors.transparent,
+                    splashColor: colors.onPrimary.splash,
+                    child: Container(
+                      width: _zoneWidth,
+                      height: double.infinity,
+                      color: colors.surfacePrimary.hover,
+                      child: Center(
+                        child: widget.loadingNext && !widget.left
+                            ? SizedBox(
+                                height: Sizes.iconSizeRegular,
+                                width: Sizes.iconSizeRegular,
+                                child: CircularProgressIndicator(
+                                  valueColor: AlwaysStoppedAnimation(
+                                    colors.textPrimary,
+                                  ),
+                                ),
+                              )
+                            : Icon(
+                                widget.left
+                                    ? Icons.arrow_back_ios_rounded
+                                    : Icons.arrow_forward_ios_rounded,
+                                size: Sizes.iconSizeLarge,
+                                color: colors.textPrimary,
+                              ),
+                      ),
+                    ),
                   ),
                 ),
               ),
